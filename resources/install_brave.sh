6c22c8df046c67d3c2bb4abf81a63327ebd8d0e2
Joseph Young <130690493+yousecjoe@users.noreply.github.com>
2024-03-08 03:53:27 -0500

    Optimize Docker environment for dev focus
    Refactored the Dockerfile to cater to a developer-focused environment by changing the HOME directory to /home/devusr and removing unused applications like Remmina and web browsers Chrome and Edge. Installation scripts for Anaconda, PyCharm, and these browsers were commented out, reducing the image size and build time. Brave browser was introduced, possibly as a lighter alternative. In the python packages installer, the pip installs targeted at aarch64 architecture were augmented with new packages, including open-interpreter, uvicorn, and fastapi, indicating a shift towards a more API-driven development setup.

---
+++ resources/install_brave.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -ex
+
+ARCH=$(uname -p)
+
+if [[ "${ARCH}" =~ ^aarch64$ ]] ; then
+    # Install Brave Browser
+    apt install curl
+
+    curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+    apt update
+
+    apt install brave-browser
+EOL
+
+else
+    # Install Brave
+
+    apt-get update
+    apt install curl
+
+    curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+    apt update
+
+    apt install brave-browser
+EOL
+
+fi
+
--- resources/install_python_packages.sh
+++ resources/install_python_packages.sh
@@ -7,7 +7,7 @@ pip3 install --upgrade pip
 if [[ "${ARCH}" =~ ^aarch64$ ]] ; then
     pip3 install numpy torch opencv-python torchvision typing \
         torchstat torchsummary ptflops onnx onnxruntime lxml \
-        scikit-image Pillow ffmpeg geopandas
+        scikit-image Pillow ffmpeg geopandas open-interpreter uvicorn fastapi
     pip3 install tensorflow -f https://tf.kmtea.eu/whl/stable.html
 else
     pip3 install numpy torch opencv-python torchvision typing \
